528468c69e934a6d46a94e1c6da1348062ff3cd1
tikhsuP <pushkit20@gmail.com>
2020-11-02 00:52:48 +0530

    Design 'Home' page header

--- google/src/App.js
+++ google/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import Home from './pages/Home';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Home>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>
 
-      </Home>
+            </h1>
+          </Route>
+          <Route path="/">
+            <Home>
+            </Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ google/src/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="home-header">
+          <div className="header-left">
+            <Link to='/about'>About</Link>
+            <Link to='/store'>Store</Link>
+          </div>
+          <div className="header-right">
+            <Link to='/gmail'>Gmail</Link>
+            <Link to='/images'>Images</Link>
+            <AppsIcon></AppsIcon>
+            <Avatar></Avatar>
+          </div>
+        </div>
+
+        <div className="home-body">
+          <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"></img>
+        </div>
+      </div>
+        )
+    }
+}
+
+export default Home
