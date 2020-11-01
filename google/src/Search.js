6dd854c1eef1288c2cbb123c4bea55810d030aab
tikhsuP <pushkit20@gmail.com>
2020-11-02 03:48:18 +0530

    Add React Context API

--- google/src/Search.js
+++ google/src/Search.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 import './Search.css';
+import { useStateValue } from './StateProvider';
+import { actionTypes } from './reducer';
 
 
-function Search() {
+function Search({ hideButtons = false }) {
+    const [{}, dispatch] = useStateValue();
+    const [input, setInput] = useState("");
+    const history = useHistory();
+
+    const search = e => {
+        e.preventDefault();
+
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        })
+
+        history.push('/search');
+    }
+
     return (
-        <div className="search">
+        <form className="search">
             <div className="search-input">
                 <SearchIcon className="search-icon"></SearchIcon>
-                <input></input>
+                <input value={input} onChange={e => setInput(e.target.value)}></input>
                 <MicIcon className="search-icon"></MicIcon>
             </div>
-            <div className="search-buttons">
-                <Button variant="outlined">Google Search</Button>
+
+            {!hideButtons ? (
+              <div className="search-buttons">
+                <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
                 <Button variant="outlined">I'm Feeling Lucky</Button>
-            </div>
-        </div>
+              </div>
+            ) : (
+              <div className="search-buttons">
+                <Button className="search-buttons-hidden" type="submit" onClick={search} variant="outlined">Google Search</Button>
+                <Button className="search-buttons-hidden" variant="outlined">I'm Feeling Lucky</Button>
+              </div>
+            )}
+        </form>
     )
 }
 
