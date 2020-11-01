49f6a7298f10bbc4a1c384809c59a11b40903adc
tikhsuP <pushkit20@gmail.com>
2020-11-01 23:43:56 +0530

    Install dependencies

---
+++ google/src/pages/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export class Home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <div clasName="home-header">
+                    <div className="header-left">
+
+                    </div>
+                    <div className="header-right">
+
+                    </div>
+                </div>
+
+                <div clasName="home-body">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
